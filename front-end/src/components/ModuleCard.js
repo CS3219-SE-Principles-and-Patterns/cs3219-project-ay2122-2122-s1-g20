3435611409df44670626a214fbce2143224a5b37
sylviaokt <kaitingx3@gmail.com>
2021-09-29 03:54:13 +0800

    Add module selection page

---
+++ front-end/src/components/ModuleCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FiX } from "react-icons/fi";
+
+const ModuleCard = ({ moduleName, onClose }) => {
+  return (
+    <div className="mt-5 px-5 py-3 rounded-2xl min-h-full text-left flex justify-center items-center relative bg-blue-light text-purple-dark">
+      {moduleName}
+      <button onClick={() => onClose(moduleName)}>
+        <FiX className="absolute h-6 w-6 p-0.5 -top-1.5 -right-1 bg-grey-light bg-opacity-36 rounded-full" />
+      </button>
+    </div>
+  );
+};
+
+export default ModuleCard;
