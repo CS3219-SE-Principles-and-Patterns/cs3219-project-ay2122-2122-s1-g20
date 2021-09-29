1bbbf56c2ec1a3d85d10f0726d384d096e1097a7
Andrea Tan <andreaxtan611@gmail.com>
2021-09-29 23:42:22 +0800

    Display error messages on UI

--- front-end/src/components/forms/LoginForm.js
+++ front-end/src/components/forms/LoginForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ErrorMessage from "../alerts/ErrorMessage";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorLogin, setErrorLogin] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
   const [isRevealPassword, setIsRevealPassword] = useState(false);
 
   const handleEmailChange = (event) => {
@@ -32,6 +34,7 @@ const LoginForm = () => {
       const responseData = await response.json();
 
       if (response.status !== 200) {
+        setAlertMessage(responseData.message);
         setErrorLogin(true);
         throw new Error(responseData.message);
       }
@@ -48,72 +51,80 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="py-6 align-middle justify-center mt-5">
-      <form
-        onSubmit={handleLogin}
-        className="space-y-6"
-        action="#"
-        method="POST"
-      >
-        <div>
-          <label
-            htmlFor="email"
-            className="block flex text-sm font-medium text-gray-700"
-          >
-            Email address
-          </label>
-          <input
-            onChange={handleEmailChange}
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            className="mt-1 appearance-none w-64 py-3 sm:w-96 border-none block pl-3 py-2 sm:py-4 rounded-md bg-purple-light focus:outline-none focus:ring-purple-dark text-xs focus:border-purple-dark sm:text-sm"
-          />
-        </div>
-        <div>
-          <div className="flex justify-between">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Password
-            </label>
-            <button
-              type="button"
-              onClick={() => setIsRevealPassword((prevState) => !prevState)}
-              className="text-purple-dark hover:underline text-xs sm:text-sm hover:bg-opacity-75"
-            >
-              Show Password
-            </button>
-          </div>
-          <input
-            value={password}
-            onChange={handlePasswordChange}
-            id="password"
-            name="password"
-            type={isRevealPassword ? "text" : "password"}
-            required
-            className="mt-1 text-xs py-3 appearance-none mb-8 w-64 sm:w-96 border-none block pl-3 py-2 sm:py-4 rounded-md bg-purple-light focus:outline-none focus:ring-purple-dark focus:border-purple-dark sm:text-sm"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-54 text-sm sm:text-md justify-center py-3 px-10 border-transparent rounded-md shadow-sm font-medium text-black bg-yellow-dark hover:bg-opacity-75 mt-6"
+    <div className="mt-5">
+      {errorLogin ? (
+        <ErrorMessage authType="logging in:" message={alertMessage} />
+      ) : undefined}
+      <div className="py-6 align-middle justify-center mt-5">
+        <form
+          onSubmit={handleLogin}
+          className="space-y-6"
+          action="#"
+          method="POST"
         >
-          Log In
-        </button>
-        <p className="text-sm pt-5 hover:underline text-purple-dark flex justify-center">
-          <Link to="/emailConfirmation">Forgot your password?</Link>
-        </p>
+          <div>
+            <label
+              htmlFor="email"
+              className="block flex text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              onChange={handleEmailChange}
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              className="mt-1 appearance-none w-64 py-3 sm:w-96 border-none block pl-3 py-2 sm:py-4 rounded-md bg-purple-light focus:outline-none focus:ring-purple-dark text-xs focus:border-purple-dark sm:text-sm"
+            />
+          </div>
+          <div>
+            <div className="flex justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Password
+              </label>
+              <button
+                type="button"
+                onClick={() => setIsRevealPassword((prevState) => !prevState)}
+                className="text-purple-dark hover:underline text-xs sm:text-sm hover:bg-opacity-75"
+              >
+                Show Password
+              </button>
+            </div>
+            <input
+              value={password}
+              onChange={handlePasswordChange}
+              id="password"
+              name="password"
+              type={isRevealPassword ? "text" : "password"}
+              required
+              className="mt-1 text-xs py-3 appearance-none mb-8 w-64 sm:w-96 border-none block pl-3 py-2 sm:py-4 rounded-md bg-purple-light focus:outline-none focus:ring-purple-dark focus:border-purple-dark sm:text-sm"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-54 text-sm sm:text-md justify-center py-3 px-10 border-transparent rounded-md shadow-sm font-medium text-black bg-yellow-dark hover:bg-opacity-75 mt-6"
+          >
+            Log In
+          </button>
+          <p className="text-sm pt-5 hover:underline text-purple-dark flex justify-center">
+            <Link to="/emailConfirmation">Forgot your password?</Link>
+          </p>
 
-        <p className="text-sm flex justify-center">
-          Do not have an account?
-          <Link className="text-purple-dark pl-1 hover:underline" to="/signup">
-            Register here
-          </Link>
-        </p>
-      </form>
+          <p className="text-sm flex justify-center">
+            Do not have an account?
+            <Link
+              className="text-purple-dark pl-1 hover:underline"
+              to="/signup"
+            >
+              Register here
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
