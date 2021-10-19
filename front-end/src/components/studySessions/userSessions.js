43ce2fb64961d675cd3e8c44913f600cf3e65bf8
Andrea Tan <andreaxtan611@gmail.com>
2021-10-19 17:12:38 +0800

    Add responsiveness

--- front-end/src/components/studySessions/userSessions.js
+++ front-end/src/components/studySessions/userSessions.js
@@ -9,24 +9,8 @@ function classNames(...classes) {
 
 export default function UserSessions() {
   return (
-    <div className="bg-blue-light h-screen ">
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-        <select
-          id="tabs"
-          name="tabs"
-          className="block focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-          defaultValue={tabs.find((tab) => tab.current).name}
-        >
-          {tabs.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div>
-      <div className="hidden justify-items-center sm:block">
+    <div className="bg-blue-light h-screen">
+      <div className="justify-items-center">
         <nav
           className="relative ml-20 mr-20 max-w-lg mt-10 z-0 rounded-lg shadow flex divide-x divide-gray-200"
           aria-label="Tabs"
@@ -41,7 +25,7 @@ export default function UserSessions() {
                   : "text-gray-500 bg-purple-light hover:text-gray-700",
                 tabIdx === 0 ? "rounded-l-lg" : "",
                 tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
-                "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-yellow-100 focus:z-10"
+                "group relative min-w-0 flex-1 bg-white py-4 px-4 text-sm font-medium text-center hover:bg-yellow-100 focus:z-10"
               )}
               aria-current={tab.current ? "page" : undefined}
             >
