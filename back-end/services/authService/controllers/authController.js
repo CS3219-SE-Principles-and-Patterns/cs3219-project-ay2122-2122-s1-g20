a714e2400217e65abf42f63a1e4d555235e24980
Kang Min Hui, Mabel <65720353+mabel-kang@users.noreply.github.com>
2021-10-21 21:26:16 +0800

    Add backend for logout

--- back-end/services/authService/controllers/authController.js
+++ back-end/services/authService/controllers/authController.js
@@ -6,6 +6,7 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 const User = require("../model/user");
 
 let transporter = nodemailer.createTransport({
@@ -89,6 +90,8 @@ async function sendVerificationMail(uniqueString, email) {
 
 exports.signup = (req, res) => {
   const { email, username, password } = req.body;
+  // generating salt to be used with jwt
+  const jwtSalt = bcrypt.genSaltSync(10);
   User.findOne({ email: email })
     .then((user) => {
       if (user) {
@@ -101,9 +104,9 @@ exports.signup = (req, res) => {
       return crypto.randomBytes(20).toString("hex");
     })
     .then((uniqueString) => {
-      const user = new User({ email, username, password, uniqueString });
+      const user = new User({ email, username, password, uniqueString, jwtSalt });
       user.save();
-      const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY);
+      const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY + jwtSalt);
       const accessToken = oAuth2Client.getAccessToken();
       const transporter = nodemailer.createTransport({
         service: "gmail",
@@ -156,8 +159,10 @@ exports.login = async (req, res) => {
 
   try {
     await user.comparePassword(password);
-    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY);
+    const jwtSalt = user.jwtSalt;
+    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY + jwtSalt);
     // Check that email is verified before logging in
+    console.log(token);
     if (user.isVerified === false) {
       return res
         .status(422)
@@ -174,6 +179,22 @@ exports.login = async (req, res) => {
       .json({ message: "Invalid password or email entered." });
   }
 };
+
+exports.logout = async (req, res) => {
+
+  const { email } = req.body;
+  try {
+    const newSalt = bcrypt.genSaltSync(10);
+    const updatedUser = await User.findOneAndUpdate(
+      { email: email },
+      { jwtSalt: newSalt },
+      { new: true }
+    );
+    return res.status(200).json({ user: updatedUser, message: "Salt changed upon logout" });
+  } catch (err) {
+    return res.status(422).json({message: "Error logging out"});
+  }
+}
 
 exports.postReset = (req, res, next) => {
   crypto
