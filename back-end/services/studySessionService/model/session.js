f698535c59a9a4b857db571f2ba096d5ca7ceaab
Andrea Tan <andreaxtan611@gmail.com>
2021-10-22 17:02:38 +0800

    Add schema

---
+++ back-end/services/studySessionService/model/session.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const studySessionSchema = mongoose.Schema({
+  sid: {
+    type: Number,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  timeLimit: {
+    type: Number,
+    required: true,
+  },
+  time: {
+    startTime: {
+      value: Number,
+      amOrPm: String,
+      required: true,
+    },
+    endTime: {
+      value: Number,
+      amOrPm: String,
+      required: true,
+    },
+    timeOffset: Number,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  module: {
+    type: String,
+    required: true,
+  },
+  participants: {
+    type: [String],
+    required: true,
+  },
+});
+
+const Session = mongoose.model("Session", studySessionSchema);
+
+module.exports = Session;
