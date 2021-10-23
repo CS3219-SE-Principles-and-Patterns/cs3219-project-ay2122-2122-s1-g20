775bcf1b9826e6d37d337ac5fa19efdf5047726c
sylviaokt <kaitingx3@gmail.com>
2021-10-23 15:49:20 +0800

    Implement get upcoming, created and past study sessions api

--- back-end/services/studySessionService/routes/sessionRoutes.js
+++ back-end/services/studySessionService/routes/sessionRoutes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 // after creating study session, call another api to update user database under 'sessions' field
 router.post("/", sessionController.createSession);
-// router.get("/upcoming", sessionController.getUpcomingSession);
+router.get("/upcoming", sessionController.getUpcomingSessions);
+router.get("/my/:uid", sessionController.getMySessions);
+router.get("/past/:uid", sessionController.getPastSessions);
+router.get("/:sid", sessionController.getASession);
 
 router.delete("/:sid", sessionController.deleteSession);
 
