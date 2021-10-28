2736ac7daf6e1dfc62374a3cf3020237378edab4
underthehai <haishanlim@gmail.com>
2021-10-28 13:08:07 +0800

    Add dockerfile for chatsvc

--- back-end/services/chatService/server.js
+++ back-end/services/chatService/server.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const routes = require("./routes/routes");
 
-dotenv.config({ path: "../../config.env" });
+dotenv.config({ path: "config.env" });
 
 const app = express();
 const PORT = process.env.CHAT_PORT || config.port;
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 
 app.use("/api", routes);
@@ -28,37 +30,37 @@ const options = {
   cors: {
     origin: "http://localhost:3000",
   },
-  transports: ['websocket'],
+  transports: ["websocket"],
   pingInterval: 1000 * 60 * 5,
-  pingTimeout: 1000 * 60 * 3
-}
+  pingTimeout: 1000 * 60 * 3,
+};
 
 const io = require("socket.io")(http, options);
 
-io.on("connection", (socket)=>{
-    console.log("user connected");
-    console.log(socket.id);
-    socket.on("send-message", (message, group) => {
-      console.log(message);
-      socket.to(group).emit("receive-message", (message));
-    })
-    socket.on("join-room", group => {
-      socket.join(group);
-    })
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket.id);
+  socket.on("send-message", (message, group) => {
+    console.log(message);
+    socket.to(group).emit("receive-message", message);
+  });
+  socket.on("join-room", (group) => {
+    socket.join(group);
+  });
 });
 
-http.listen(PORT, ()=>{
-    console.log("connected to port: " + PORT)
+http.listen(PORT, () => {
+  console.log("connected to port: " + PORT);
 });
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-  
+
 mongoose
-    .connect(DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB connection successful!"));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
