e3a89a6f648a824b3eafb197c808360f065a0db9
Andrea Tan <andreaxtan611@gmail.com>
2021-11-01 16:45:44 +0800

    cd: Trigger build

--- front-end/src/components/alerts/SessionAlerts.js
+++ front-end/src/components/alerts/SessionAlerts.js
@@ -12,7 +12,6 @@ export default function SessionAlerts(props) {
         className="fixed inset-0 z-50 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
       >
         <div className="w-full z-50 flex flex-col items-center space-y-4 sm:items-end">
-          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
           <Transition
             show={props.show}
             as={Fragment}
