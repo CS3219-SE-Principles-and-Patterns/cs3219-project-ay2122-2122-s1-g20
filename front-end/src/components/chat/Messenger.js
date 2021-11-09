18023395cefda4ed8476e4bdff316a05ce49e265
underthehai <haishanlim@gmail.com>
2021-11-09 22:27:04 +0800

    Update api calls

--- front-end/src/components/chat/Messenger.js
+++ front-end/src/components/chat/Messenger.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import StudyHeader from "../header/StudyHeader";
 import ChatBubble from "../bubble/ChatBubble";
 import { socket } from "./Socket";
+import { chatApi } from "../../utils/api";
 
 const Messenger = ({ account, displayChat, enable, disabled }) => {
   const [message, setMessage] = useState("");
@@ -69,34 +70,16 @@ const Messenger = ({ account, displayChat, enable, disabled }) => {
       setMessage("");
       setOldMessages(oldMessages.concat(newMessage));
 
-      const res = await fetch(
-        "https://39t21kptu5.execute-api.ap-southeast-1.amazonaws.com/v1/api/messages",
-        {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify(newMessage),
-        }
-      );
-      console.log(res);
+      await chatApi
+        .post("/messages", {
+          newMessage,
+        })
+        .catch((err) => console.log(err));
 
       //update last modified
-      try {
-        const res = await fetch(
-          `https://39t21kptu5.execute-api.ap-southeast-1.amazonaws.com/v1/api/groups/${group}`,
-          {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify(newMessage),
-          }
-        );
-        console.log(res);
-      } catch (err) {
-        console.log(err);
-      }
+      await chatApi
+        .post(`/groups/${groups}`, newMessage)
+        .catch((err) => console.log(err));
     }
   };
 
